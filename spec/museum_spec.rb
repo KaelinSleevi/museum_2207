142f6d4ddef38c15e0826d29b5df6dd06f86249c
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-07-27 09:23:15 -0600

    add tests for iteration 2: museum.

---
+++ spec/museum_spec.rb
@@ -0,0 +1,76 @@
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+require 'rspec'
+
+RSpec.describe Museum do
+  it 'exists' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    expect(dmns).to be_an_instance_of(Museum)
+  end
+
+  it 'has a name' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+  end
+
+  it 'has no exhibits by default' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    expect(dmns.exhibits).to eq([])
+  end
+
+  it 'can add exhibits' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
+
+  it 'can have patrons' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    patron_1 = Patron.new("Bob", 20)
+    patron_2 = Patron.new("Sally", 20)
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2.add_interest("IMAX")
+
+    expect(dmns.patrons).to eq([patron_1, patron_2])
+  end
+
+  it 'can recommended exhibits' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    patron_1 = Patron.new("Bob", 20)
+    patron_2 = Patron.new("Sally", 20)
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2.add_interest("IMAX")
+
+    expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
+    expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+  end
+end
